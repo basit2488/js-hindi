477391e09e2528d2914174755b4dc77b3430c9bb
basit2488 <113307714+basit2488@users.noreply.github.com>
2024-04-23 14:57:11 +0000

    Number_Math

---
+++ 01_basics/Number_Math.js
@@ -0,0 +1,19 @@
+// console.log("Number and Math");
+
+let score = 400;
+// console.log(score);
+
+let accBalance = new Number(100.90)
+// console.log(accBalance.toPrecision(2));
+
+let balance = 10000000
+// console.log(balance.toLocaleString('en-PK'));
+
+let min = 10
+let max = 20
+
+let randomMinMax = Math.round(Math.random() * (max - min + 1)) + min;
+// console.log(randomMinMax)
+
+console.log(Math.random())
+console.log(Math.random() * 10 + 1)
